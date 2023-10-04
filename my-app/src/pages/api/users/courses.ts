469c0d4682234a702c55fb9d7f8439665f6ecf25
sciencenawaaz <sciencenawaaz@gmail.com>
2023-10-04 13:37:35 +0530

    courses get req done

--- my-app/src/pages/api/users/courses.ts
+++ my-app/src/pages/api/users/courses.ts
@@ -2,6 +2,9 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import dbConnect from '@/config/connectMongoDB';
 import Course from "../../../models/courseModel"
+import { User } from '@/models/userModel';
+
+
 
 type Data = {
   result?:{
@@ -10,8 +13,24 @@ type Data = {
     price: Number,
     imageLink: String,
     published: Boolean
-  }
+  } 
+  message?:string
   error?:string
+}
+
+type TCourse = {
+  _id:String
+  title: String,
+  description: String,
+  price: Number,
+  imageLink: String,
+  published: Boolean
+}
+
+type user = {
+  username: string,
+  password: string,
+  purchasedCourses:TCourse[]
 }
 
 export default async function handler(
@@ -30,6 +49,32 @@ switch (method) {
       res.status(400).send({ error: 'failed to fetch data' })
     }
     break;
-}
+
+  case "POST": 
+    try {
+      const course = await Course.findById(req.body.courseId);
+     
+      
+      if(course) {
+        const user:user = await User.findOne({username : req.body.user.username})
+        
+        if(user){
+          user.purchasedCourses.push(course)
+          await user.save();
+        
+          return res.json({ message: 'Course purchased successfully' });
+        } else {
+          return res.status(403).json({ error: 'User not found' });
+        } 
+      } else {
+        return res.status(404).json({ error: 'Course not found' });
+      }
+      }   catch (err) {
+      res.status(400).send({ error: 'failed to fetch data' })
+    }
+    
+       break;
+  }
+
   
 }
--- my-app/src/pages/courses.tsx
+++ my-app/src/pages/courses.tsx
@@ -2,36 +2,57 @@
 
 import { useSession } from "next-auth/react"
 const axios = require('axios')
-import {useState , useEffect} from "react"
-let id = 0;
+import {useState , useEffect, useRef} from "react"
 
 type TCourse = {
-   title: String,
+   _id:string,
+   title: string,
    description: String,
-   price: Number,
+   price?: string,
    imageLink: String,
-   published: Boolean
+   published?: Boolean
 }
 
 const courses = () => {
    
    const session = useSession();
-   const authUser = session.data?.user?.email
-   console.log(JSON.stringify(authUser));
-   
+   const authUser = session.data?.user?.email //getting user session details
+   // console.log(JSON.stringify(authUser));
+
+   const [Courses, setCourses] = useState<TCourse[]>()
+   const [Loading, setLoading] = useState(true)
+   const ref = useRef(null)
    async function getCourses() {
       
-      const response = await axios.post("http://localhost:3000/api/users/purchasedCourses",{
+      const response = await axios.get("http://localhost:3000/api/users/courses",{
          headers: {
             "Content-Type": "application/json",
          },
          "username": authUser,
       }
       );
-      console.log(response);
       
+      let courses = response.data.result;
+
+      setCourses(courses) 
+      setLoading(false)
       return;
    };
+
+   const buyCourse = async () => {
+      console.log(ref.current.id);
+      
+            const response = await axios.post("http://localhost:3000/api/users/courses",{
+               headers: {
+                  "Content-Type": "application/json",
+               },
+               "username": authUser,
+               "courseId": "64ca3e8521d3d69fb9815ed1",
+               "user": {
+                     "username":authUser
+               }
+            });
+   }
    
    useEffect(() => {
      getCourses()
@@ -40,46 +61,31 @@ const courses = () => {
      }
    }, [])
 
-
    
-  if(session) {    
+  if(Loading === false) {    
 
-  return (
-    <section className=" flex flex-col justify-center items-center gap-10 p-3">
-      {/* <button onClick={getCourses}>Click me</button> */}
-        <div className="flex flex-col justify-center items-center shadow-xl shadow-amber-600 w-2/3 p-2 rounded-md hover:shadow-amber-300 lg:w-1/3">
-         <img src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg"/>
-         <h2 className=" text-center">
-            title
-         </h2>
-         <p >
-            descrripton
-         </p>
-       </div>
-       
-       <div className="flex flex-col justify-center items-center shadow-xl shadow-amber-600 w-2/3 p-2 rounded-md hover:shadow-amber-300 lg:w-1/3">
-         <img src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg"/>
-         <h2 className=" text-center">
-            title
-         </h2>
-         <p >
-            descrripton
-         </p>
-       </div>
-       <div className="flex flex-col justify-center items-center shadow-xl shadow-amber-600 w-2/3 p-2 rounded-md hover:shadow-amber-300 lg:w-1/3">
-         <img src="https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg"/>
-         <h2 className=" text-center">
-            title
-         </h2>
-         <p >
-            descrripton
-         </p>
-       </div>
+  return (Courses?.map((course,_id)=> {
+return (
+<section ref={ref} id={course._id} key={course._id}  className=" flex flex-col justify-center items-center gap-10 p-3">
+           <div className="flex flex-col justify-center items-center shadow-xl shadow-amber-600 w-2/3 p-2 rounded-md hover:shadow-amber-300 lg:w-1/3">
+            <img src = {course.imageLink} alt="course image"/>
+            <h2 className=" text-center">
+             {course.title} 
+            </h2>
+            <p >
+             {course.description} 
+            </p>
+            <span className= "py-4 text-center text-xl">Price : ₹{course.price}</span>
+         <button className="px-4 py-2 border border-slate-200 rounded-md" onClick={buyCourse}>Click me</button>
+          </div>
+       </section>
+)
 
-    </section>
-       
+  })
   )
-  }
+         
+   }
+   
   {
    return <p>Loading....</p>
   }
